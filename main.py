24847906e1f8d4ceed2b059b514c11f69fa27d4f
aniragn <anir.agountaf@lcieducation.net>
2025-06-05 12:08:57 +0100

    main.py env loading

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-# main.py
-# This file is the main entry point for the FastAPI application.
-# Author: Yassine Amounane
+import os
 from fastapi import FastAPI
 from app.api import router
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 
+load_dotenv()
+
+REACT_HOST = os.getenv("REACT_HOST")
 
 app = FastAPI(title="Silicon Shoring API - AI Agent")
 app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[REACT_HOST],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
