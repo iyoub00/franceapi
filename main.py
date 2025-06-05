7f4c90447458dade3e603902b1d347abbe17527d
HP <meryem_elg@outlook.fr>
2025-06-05 11:59:13 +0100

    Adding CORS config

--- main.py
+++ main.py
@@ -3,9 +3,19 @@
 # Author: Yassine Amounane
 from fastapi import FastAPI
 from app.api import router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Silicon Shoring API - AI Agent")
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     import uvicorn
